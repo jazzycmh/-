2c5bd93f2fdeb46e6d0091c06be07feef1d6cc49
jazzycmh <zhaziraaigeldieva@gmail.com>
2026-02-05 18:12:55 +0500

    Add files via upload

---
+++ Счастливый билет.py
@@ -0,0 +1,7 @@
+n = input().strip()
+n1 = int(n[0])+int(n[1])+int(n[2])
+n2 = int(n[3])+int(n[4])+int(n[5])
+if n1 == n2:
+    print("YES")
+else:     
+    print("NO")                                                                                     
